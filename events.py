9039ce2eb9c8f0085f28d049d2f7b4034710f9d6
David Langerveld <david.langerveld@student.auc.nl>
2016-04-05 14:29:48 +0200

    added an events python file. Contains all functions that can be used by events.

---
+++ events.py
@@ -0,0 +1,19 @@
+
+
+#Function randomly determines whether you are arrested, or get to walk free.
+def cointoss(probability):
+
+
+#Can be used to either increase or decrease player's amount of bitcoin
+def change_bitcoin(amount)
+
+
+#Can be used to either increase or decrease player's amount of dollars.
+def change_dollars(amount)
+
+
+#Used to change gamestate to arrested state
+def arrested()
+
+#Used to change gamestate to dead state (endgame)
+def killed()
