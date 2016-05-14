3e2979996a30154899db174846531c0c596fa85b
jon <jon.sotola@gmail.com>
2016-05-14 13:09:42 +0200

    Shop change

--- shop_function.py
+++ shop_function.py
@@ -3,10 +3,12 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
 from pandas import read_csv
+import numpy as np
+
 
 
 def load_events():
-    list_events = read_csv('shop_inventory.csv', delimiter=';')
+    list_events = np.genfromtxt('shop_inventory.csv', delimiter=',', dtype=np.str_)
     print("Loaded shop")
     return list_events
 
@@ -15,15 +17,16 @@ def shop(self, main):
 
     #load items to shop
     main.items = load_events()
-    print(main.items)
+
 
     label = Label(text="What would you like to buy?")
     main.add_widget(label)
 
     box = BoxLayout()
     main.add_widget(box)
-    for i in range(5):
-        item = Button(text="Item " + str(i))
+
+    for i in range(1,main.items.shape[0]):
+        item = Button(text=(main.items[i,0]+"\n" ))
         box.add_widget(item)
     button = Button(text="You can click here, but it does nothing.")
     main.add_widget(button)
