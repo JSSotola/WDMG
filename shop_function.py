e48ae8f21de5959b00ed31d7fc66c7e9120e4e72
jon <jon.sotola@gmail.com>
2016-05-14 12:41:55 +0200

    Comment

---
+++ shop_function.py
@@ -0,0 +1,28 @@
+
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from pandas import read_csv
+
+
+def load_events():
+    self.list_events = read_csv('shop_inventory.csv', delimiter=';')
+    print("Loaded shop")
+
+
+def shop(self, main):
+    main.clear_widgets()  # clears the main window
+
+    #load items to shop
+    shop.items = load_events()
+
+    label = Label(text="What would you like to buy?")
+    main.add_widget(label)
+
+    box = BoxLayout()
+    main.add_widget(box)
+    for i in range(5):
+        item = Button(text="Item " + str(i))
+        box.add_widget(item)
+    button = Button(text="You can click here, but it does nothing.")
+    main.add_widget(button)
