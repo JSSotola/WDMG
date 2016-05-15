f766f3b4d541dea6f18cedd435ebd01cf1977389
jon <jon.sotola@gmail.com>
2016-05-15 14:26:04 +0200

    Remove unneeded dependencies

--- TOR.py
+++ TOR.py
@@ -6,8 +6,6 @@ from kivy.uix.popup import Popup
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.clock import Clock
-import time
 
 def TOR(main, trigger):
 
